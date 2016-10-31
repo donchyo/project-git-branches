a2310e63b4a0691b6b375f6796df0efabe4566c0
donchyo <daniel.eke.eriksson@gmail.com>
2016-10-31 14:48:32 +0100

    commit from master,i added a print line in the main method

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("this line i added on the dev branch");
-
+		System.out.println("this line was added from the master branch");
 	}
 
 }
