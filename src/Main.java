8a91632b4a7a763d29082a3fb8a9c1fc4188f9d1
donchyo <daniel.eke.eriksson@gmail.com>
2016-10-31 14:59:07 +0100

    added some shit so we have something to merge

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("this line i added on the dev branch");
 		System.out.println("this line was added from the master branch");
+		System.out.println("this line i added from the dev branch just ot have something to merge");
 	}
 
 }
